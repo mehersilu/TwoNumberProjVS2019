6b22f641725ff5856918447710727b846950db4b
mehersilu <meher.silu@gmail.com>
2023-01-30 12:53:50 +0530

    Commit1

---
+++ TwoNumberProj/TwoNumberProj/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TwoNumberProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c,b = 4, a = 6;
+
+            c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
